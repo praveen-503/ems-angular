5d1cb9037c1b2db7a998c5d99f2902e1f1e5aa9c
praveenb <praveeb@proarch.com>
2020-02-19 17:37:55 +0530

    0005-P modules changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',loadChildren:'./modules/home/home.module#HomeModule'},
   {path:'enroll',loadChildren:'./modules/enroll/enroll.module#EnrollModule'},
-  
+  {path:'billing-manager',loadChildren:'./modules/billing-manager/billing-manager.component#BillingManagerComponent'}
 ];
 
 @NgModule({
