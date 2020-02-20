b17a92ec1d68955464402bacbeebeb65753804c8
harichandanam <harichandanam@proarch.com>
2020-02-20 16:18:53 +0530

    0007-C

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-   
+    ReactiveFormsModule,
+    FormsModule,
     BillingManagerModule,
     HttpClientModule
   ],
--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin.component';
+import { AddClientComponent } from './add-client/add-client.component';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { ViewEmployeeDisignationComponent } from './view-employee-disignation/view-employee-disignation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AdminComponent,
+   children:[{path:'add-client',component:AddClientComponent},
+  {path:'add-project',component:AddProjectComponent},
+{path:'view-employee',component:ViewEmployeeDisignationComponent}]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
