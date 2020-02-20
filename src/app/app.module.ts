1a81268d9708468602802b69a2c7d17da3badf79
praveenb <praveeb@proarch.com>
2020-02-20 11:53:42 +0530

    0006-P some changes in registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule,FormsModule } from '@angular/forms';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
@@ -26,7 +25,6 @@ import { NotFoundComponent } from './shared/components/not-found/not-found.compo
     ReactiveFormsModule,
     BillingManagerModule,
     HttpClientModule
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/enroll/registration/registration.component.ts
+++ src/app/modules/enroll/registration/registration.component.ts
@@ -12,18 +12,36 @@ export class RegistrationComponent implements OnInit {
 
   registerForm: FormGroup;
   submitted = false;
-
+  projectList = [
+    { Id: 1, Name: 'Angular 2+' },
+    { Id: 2, Name: 'Angular 4' },
+    { Id: 3, Name: 'Angular 5' },
+    { Id: 4, Name: 'Angular 6' },
+    { Id: 5, Name: 'Angular 7' }
+  ];
   constructor(private formBuilder: FormBuilder,
     private router: Router
   ) { }
 
   ngOnInit() {
+
+//     var des = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value');
+// Object.defineProperty(HTMLInputElement.prototype, 'value', { get: function() {
+//   if(this.type === 'text' && this.list) {
+//     var value = des.get.call(this);
+//     var opt = [].find.call(this.list.options, function(option) {
+//       return option.value === value;
+//     });
+//     return opt ? opt.dataset.value : value;
+//   }
+// }});
     this.registerForm = this.formBuilder.group({
       EmpID: ['', Validators.required,],
       FirstName: ['', Validators.required],
       LastName: ['', Validators.required],
       Password: ['', [Validators.required, Validators.minLength(6)]],
       ConfirmPassword: ['', Validators.required],
+      Project: ['', Validators.required],
       acceptTerms: [false, Validators.requiredTrue]
 
     },
@@ -39,8 +57,11 @@ export class RegistrationComponent implements OnInit {
     this.submitted = true;
     // stop here if form is invalid
     if (this.registerForm.invalid) {
+      console.log(this.registerForm.value);
       return;
     }
+
+    console.log(this.registerForm.value);
     // this.empService.onPostemployee(this.registerForm.value as Employee)
     //   .subscribe(data => {
     //     if (data.Status == 'Error') {
