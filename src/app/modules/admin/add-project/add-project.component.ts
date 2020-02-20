2b899bf875c669b3cc5ec493de9a65937cae644d
harichandanam <harichandanam@proarch.com>
2020-02-20 17:55:13 +0530

    0009-C changes in admin module

--- src/app/modules/admin/add-project/add-project.component.ts
+++ src/app/modules/admin/add-project/add-project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-project',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor() { }
+  addForm:FormGroup;
+
+  submitted=false;
+  public codeValue: string;
+
+  clientList = [
+    { Id: 1, Name: 'SEDC' },
+    { Id: 2, Name: 'angular' },
+    { Id: 3, Name: 'Angular 5' },
+    { Id: 4, Name: 'Angular 6' },
+    { Id: 5, Name: 'Angular 7' }
+  ];
+
+  // public saveCode(e): void {
+  //   let name = e.target.value;
+  //   let list = this.clientList.filter(x => x.name === name)[0];
+  //   console.log(list.id);
+  // }
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+   this.addForm=this.formBuilder.group(
+     {
+      projectname:['',Validators.required],
+      addclient:['']
+     }
+   )
+    
+  
+  }
+  get f() {
+    return this.addForm.controls;
+  }
+  onSubmit(){
+    this.submitted=true;
+    if(this.addForm.invalid){
+      console.log(this.addForm.value);
+      return;
+    }
+   
+    console.log(this.addForm.value);
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.addForm.value));
+    this.submitted=false;
+
+    this.addForm.reset();
+  }
+  onReset(){
+    this.addForm.reset();
   }
 
 }
