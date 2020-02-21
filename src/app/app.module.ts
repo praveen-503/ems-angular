c61e46300b9e49eeb3c903c512952e0b933fc2b6
ashar <ashar@proarch.com>
2020-02-21 12:05:18 +0530

    0003 -simple changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,6 @@ import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< .mine
-import { AdminModule } from './modules/admin/admin.module';
-import { AdminComponent } from './modules/admin/admin.component';
-=======
-import { AdminModule } from './modules/admin/admin.module';
-
->>>>>>> .theirs
-
 
 @NgModule({
   declarations: [
@@ -23,16 +15,12 @@ import { AdminModule } from './modules/admin/admin.module';
     NavBarComponent,
     FooterComponent,
     NotFoundComponent,
-    AdminComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
    
-    AdminModule
-    AdminModule
-    
     
   ],
   providers: [],
